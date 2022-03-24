08a9b9ddb60405c051b39e7ec54a143b45b780bf
Aypraise123 <102253286+Aypraise123@users.noreply.github.com>
2022-03-24 08:00:09 +0100

    Update and rename Aypraise123 to server.py

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-pyramid==2.0
-
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
@@ -20,3 +18,4 @@ if __name__ == '__main__':
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
+
